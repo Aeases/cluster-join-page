f8273e280b84e2f01d04933650487432b72f61a0
Aeases <96164686+Aeases@users.noreply.github.com>
2023-01-08 01:02:53 +0800

    Visual Fixes + Added Notice for Frozen Games

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={cn("flex-row flex flex-wrap gap-2")}>
+        <p className="font-bold text-red-500 p-3 mb-3 font-sans text-lg">Game may appear frozen while joining.</p>
+        <div className={cn(styles.center, "flex-col gap-4 rotate-90")}/> 
+
+        <div className={cn("flex-row flex flex-wrap gap-2 my-2")}>
+          
         <a>
           <div className={cn(styles.thirteen, "transition=all ring-0 hover:ring-2 ring-gray-500 transition-all duration-200 my-3 bg-clip-content cursor-not-allowed bg-linear-gradient-to-br from-[rgb(239,68,68,1)] to-[rgb(239,68,68,1)]")}>
             Join Quartex - Island
@@ -81,13 +85,13 @@ export default function Home() {
           </div>
           </a>
         </div>
-        <div className={cn(styles.center, "flex-col gap-4")}/> 
-        <div className="flex flex-col border-slate-600 border-4 rounded-lg hover:border-green-500 transition-colors duration-150 w-full max-w-5xl m-4">
+        <div className="flex flex-col border-slate-600 border-4 rounded-lg hover:border-green-500 transition-colors duration-150 w-full max-w-5xl">
           <div className="flex gap-2">
-          <button className={cn('bg-[#1e1e1e] ring-0 enabled:hover:ring-2 ring-gray-400 py-1 px-2 flex-grow')} disabled={fileName === "gameini"} onClick={() => setFileName("gameini")}>Game Settings</button>
-          <button className={cn('bg-[#1e1e1e] ring-0 enabled:hover:ring-2 ring-gray-400 py-1 px-2 flex-grow')} disabled={fileName === "gusini"} onClick={() => setFileName("gusini")}>Game User Settings</button>
+          <button className={cn('bg-[#1e1e1e] ring-0 active:bg-[#3a3a3a]  disabled:bg-[#272727] enabled:hover:ring-2 ring-gray-400 py-1 px-2 flex-grow shadow-md transition-colors ')} disabled={fileName === "gameini"} onClick={() => setFileName("gameini")}>Game Settings</button>
+          <button className={cn('bg-[#1e1e1e] ring-0 active:bg-[#3a3a3a] disabled:bg-[#272727] enabled:hover:ring-2 ring-gray-400 py-1 px-2 flex-grow shadow-md transition-colors')} disabled={fileName === "gusini"} onClick={() => setFileName("gusini")}>Game User Settings</button>
           </div>
-        <div className="w-full h-full">
+        <div className="w-full h-full flex justify-center">
+        <div className={cn(styles.center, "flex-col gap-4")}/> 
         <Editor
           height="50vh"
           width="100%"
