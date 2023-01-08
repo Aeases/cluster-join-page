adbab90e5d8c3a9994b4ddddc02f51a5eb15f03c
Aeases <96164686+Aeases@users.noreply.github.com>
2023-01-08 14:31:31 +0800

    Changed from font sizing to direct sizing with size attribute on icon

--- components/ServerButtonComponent.tsx
+++ components/ServerButtonComponent.tsx
@@ -13,7 +13,7 @@ export default function ServerButtonComponent({ Locked, IP, queryport, ServerNam
         <a href={Locked ? undefined : `steam://connect/${Address}/${password}`}>
           <div className={cn(Locked && 'cursor-not-allowed bg-[#101010] text-[#101010] select-none', "relative transition=all w-full text-center text-2xl rounded-md flex-grow ring-0 hover:ring-2 ring-gray-500 bg-[#1e1e1e] p-3 transition-all duration-200 my-3 mb-0 bg-linear-gradient-to-br from-[rgb(239,68,68,1)] to-[rgb(239,68,68,1)]")}>
             {ServerName}
-            {Locked && (<GiCombinationLock className="text-4xl absolute left-0 right-0 top-0 bottom-0 my-auto mx-auto text-gray-300"/>)}
+            {Locked && (<GiCombinationLock size="2.5em" className="text-base absolute left-0 right-0 top-0 bottom-0 my-auto mx-auto text-gray-300"/>)}
           </div>
         </a>
     )
