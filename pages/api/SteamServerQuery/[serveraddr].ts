49a3145d05c82a098a1e481071ad1fb1a984ce69
Aeases <96164686+Aeases@users.noreply.github.com>
2023-01-09 23:43:17 +0800

    Good State of [serveraddr].ts can verify if input is IP@PORT format, and return something! now to apply steam server check

--- pages/api/SteamServerQuery/[serveraddr].ts
+++ pages/api/SteamServerQuery/[serveraddr].ts
@@ -6,5 +6,26 @@ export default function handler(
     res: NextApiResponse,
   ) {
 const { serveraddr } = req.query
-res.status(200).json({message: `${serveraddr}`})
+if (typeof serveraddr !== 'string') {
+    res.status(400).json({message: `Your query is aids it ain't a string so u musta done something weird, the format should be IP@PORT e.g. 0.0.0.0@25565 `})
+}
+const AddressUnparsed = serveraddr as string
+const Address = AddressUnparsed.split('@')
+const IPRegExp = '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$' // Source https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
+const PortRegExp = '^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$' // Source https://ihateregex.io/expr/port/
+const IP = () => {
+    if (Address[0].match(IPRegExp)) {
+        return(Address[0])
+    } else res.status(400).json({message: `Bad Query, No IP Provided.`})
+}
+const Port = () => {
+    if (Address[1].match(PortRegExp)) {
+        return(Address[1])
+    } else res.status(400).json({message: `Bad Query no Port Provided`})
+}
+if (!{IP, Port}) {
+    res.status(400).json({message: `Bad Query, No IP or Port Provided`})
+}
+
+res.status(200).json({message: `${serveraddr} oh btw ${IP()} and ${Port()}`})
 }
