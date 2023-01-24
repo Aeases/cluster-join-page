86eb8e1da3d5fa95a5bc5679342782d8848e077c
Aeases <96164686+Aeases@users.noreply.github.com>
2023-01-24 19:55:55 +0800

    Refactored Props to make (sort of) type safe, also made GameListItem defined with const instead of function

--- components/GameListItem.tsx
+++ components/GameListItem.tsx
@@ -1,6 +1,14 @@
 import React from "react"
 
-export default function GameListItem({imageurl, name, path}: {imageurl: string, name: string, path: string | undefined}) {
+interface GameListItemProps {
+    imageurl: string,
+    name: string,
+    path?: string
+}
+
+
+const GameListItem = (props: GameListItemProps) => {
+    const {imageurl, name, path} = props
     return(
     <a href={path && `${path}`}>
         <div className="my-2 mx-auto h-14 flex bg-[#1e1e1e] active:bg-[#3a3a3a] active:ring-1 p-2 rounded-lg hover:ring-2 ring-gray-500 transition-all duration-200">
@@ -11,3 +19,5 @@ export default function GameListItem({imageurl, name, path}: {imageurl: string, 
 
     )
 }
+
+export default GameListItem
